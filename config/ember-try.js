7ea45307b87150520ceaee3b5de932dc41b173e2
Jose Luis Represa <joseluis.represa@bbva.com>
2018-04-10 16:00:59 +0200

    fix linter + .travis.yml

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,77 +1,91 @@
-/*jshint node:true*/
+/* eslint-env node */
 module.exports = {
-  scenarios: [
-    {
-      name: 'default',
-      bower: {
-        dependencies: { }
-      }
-    },
-    {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
-      }
-    },
-    {
-      name: 'ember-2.11',
-      bower: {
-        dependencies: {
-          'ember': '~2.11.0'
-        },
-        resolutions: {
-          'ember': '~2.11.0'
-        }
-      }
-    },
-    {
-      name: 'ember-2.12',
-      bower: {
-        dependencies: {
-          'ember': '~2.12.0'
-        },
-        resolutions: {
-          'ember': '~2.12.0'
-        }
-      }
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      }
-    }
-  ]
+	scenarios: [
+		{
+			name: 'ember-lts-2.4',
+			bower: {
+				dependencies: {
+					ember: 'components/ember#lts-2-4'
+				},
+				resolutions: {
+					ember: 'lts-2-4'
+				}
+			},
+			npm: {
+				devDependencies: {
+					'ember-source': null
+				}
+			}
+		},
+		{
+			name: 'ember-lts-2.8',
+			bower: {
+				dependencies: {
+					ember: 'components/ember#lts-2-8'
+				},
+				resolutions: {
+					ember: 'lts-2-8'
+				}
+			},
+			npm: {
+				devDependencies: {
+					'ember-source': null
+				}
+			}
+		},
+		{
+			name: 'ember-release',
+			bower: {
+				dependencies: {
+					ember: 'components/ember#release'
+				},
+				resolutions: {
+					ember: 'release'
+				}
+			},
+			npm: {
+				devDependencies: {
+					'ember-source': null
+				}
+			}
+		},
+		{
+			name: 'ember-beta',
+			bower: {
+				dependencies: {
+					ember: 'components/ember#beta'
+				},
+				resolutions: {
+					ember: 'beta'
+				}
+			},
+			npm: {
+				devDependencies: {
+					'ember-source': null
+				}
+			}
+		},
+		{
+			name: 'ember-canary',
+			bower: {
+				dependencies: {
+					ember: 'components/ember#canary'
+				},
+				resolutions: {
+					ember: 'canary'
+				}
+			},
+			npm: {
+				devDependencies: {
+					'ember-source': null
+				}
+			}
+		},
+		{
+			name: 'ember-default',
+			npm: {
+				devDependencies: {}
+			}
+		}
+	]
 };
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -2,11 +2,10 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
-Router.map(function() {
-});
+Router.map(() => {});
 
 export default Router;
